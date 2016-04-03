d53cf3c9be352297ad21f0955147ffecbb9aaa02
Daan de Jong <daanvulkaan@gmail.com>
2016-04-03 22:03:00 +0200

    Modify the network so that the number of hidden layers is adjustable

--- lib/NeuralNet.rb
+++ lib/NeuralNet.rb
@@ -1,6 +1,8 @@
 require_relative "neuralnet/network.rb"
 require "NMatrix"
 
+srand(1)
+
 x = N[  [0,0,1],
         [0,1,1],
         [1,0,1],
@@ -13,7 +15,7 @@ test_set = N[ [0,0,1],
               [1,0,1],
               [1,1,1], dtype: :float64 ]
 
-nn = Network.new(3, training_input: x, training_output: y)
-nn.train(1000)
+nn = Network.new(3, 0, training_input: x, training_output: y)
+nn.train(10000)
 puts nn.calculate(test_set)
 puts nn.result
--- lib/neuralnet/network.rb
+++ lib/neuralnet/network.rb
@@ -1,40 +1,67 @@
 class Network
-  attr_accessor :number_inputs, :training_input, :training_output, :syn0, :calculation_input
+  attr_accessor :number_inputs, :training_input, :training_output, :syn0,
+  :calculation_input, :number_hiddenlayers
 
-  def initialize(number_inputs, opts = {})
+  def initialize(number_inputs, number_hiddenlayers, opts = {})
     @number_inputs = number_inputs
+    @number_hiddenlayers = number_hiddenlayers
     @training_input = opts[:training_input]
     @training_output = opts[:training_output]
     @calculation_input = opts[:calculation_input]
-
+    # create synapse array
+    @syn = []
+    # create layer arrays
+    @l = []
+    @l_error = []
+    @l_delta = []
     # initialize weights randomly
-    @syn0 = N.random([@number_inputs, 1]) * 2 - 1
+    (0..@number_hiddenlayers).each do |number|
+      @syn[number] = N.random([@number_inputs, 1]) * 2 - 1
+    end
   end
 
   def train(iterations)
-    (0..iterations).each do
-      # forward propagation
-      l0 = @training_input
-      l1 = nonlin(l0.dot @syn0)
+    (0..iterations).each do |x|
+      @l[0] = @training_input
 
-      # how much did we miss?
-      l1_error = @training_output - l1
+      # an entire pass through the network
+      (0..@number_hiddenlayers).each do |curr_l|
+        # forward propagation
+        @l[curr_l+1] = nonlin(@l[curr_l].dot @syn[curr_l])
+      end
+
+      # how much did we mis the target output?
+      @l_error[@number_hiddenlayers+1] = @training_output - @l[@number_hiddenlayers+1]
 
       # multiply how much we missed by the
       # slope of the sigmoid at the values in l1
-      l1_delta = l1_error * nonlin(l1, true)
+      @l_delta[@number_hiddenlayers+1] = @l_error[@number_hiddenlayers+1] *
+                                      nonlin(@l[@number_hiddenlayers+1], true)
 
-      # update weights
-      @syn0 += l0.transpose.dot l1_delta
+      # update the synapse connecting the last two layers
+      @syn[@number_hiddenlayers] += @l[@number_hiddenlayers].transpose.dot @l_delta[@number_hiddenlayers+1]
+
+      # back propagation for the remaining layers
+      if @number_hiddenlayers > 0
+        (@number_hiddenlayers..0).each do |curr_l|
+          # how much did we miss?
+          @l_error[curr_l] = @l_delta[curr_l+1].dot @syn[curr_l].transpose
+          # multiply how much we missed by the
+          # slope of the sigmoid at the values in l1
+          @l_delta[curr_l] = @l_error[curr_l] * nonlin(@l[curr_l], true)
+          # update weights
+          @syn[curr_l] += @l[curr_l].transpose.dot @l_delta[curr_l]
+        end
+      end
     end
   end
 
   def result
-    @syn0
+    @syn[0]
   end
 
   def calculate(l0 = @calculation_input)
-    l1 = nonlin(l0.dot @syn0)
+    l1 = nonlin(l0.dot @syn[0])
     return l1
   end
 
